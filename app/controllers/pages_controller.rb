0a1217b3f56995eb070ff32d74775fa97090b3b1
Christel Mogollon <christel.floresm@gmail.com>
2019-01-31 13:02:27 -0500

    edited forms and blogs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,7 @@ class PagesController < ApplicationController
       @user_blogs = Blog.where(user_id: @user.id)
       @blogs = Blog.all
       @chats = Chat.all
+      @user_lessons = Lesson.where(level_id: @user.level_id)
     end
   end
 end
